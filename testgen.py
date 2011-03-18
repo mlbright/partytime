a46bc84b3ecc55977186a474f024e7f399acb41e
Martin-Louis Bright <mlbright@gmail.com>
2011-03-17 23:36:36 -0400

    generate test case

--- testgen.py
+++ testgen.py
@@ -1,15 +1,45 @@
 import sys
-import optparse
-import random
+from random import randint
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 if __name__ == "__main__":
     T = 1
-    F = random.randint(1,11)
-    N = random.randint(F+1,250)
-    M = random.randint(N-1,N*(N-1)/2)
+    F = randint(1,11)
+    N = randint(F+1,250)
+    M = randint(N-1,N*(N-1)/2)
     G = nx.generators.random_graphs.gnm_random_graph(N,M)
-    print "F=%d N=%d M=%d" % (F,N,M)
+    #for i in xrange(0,G.number_of_nodes()):
+    #    G.node[i]['cost'] = randint(0,1000)
+    #print "F=%d N=%d M=%d" % (F,N,M)
     nx.draw(G)
     plt.savefig("ex.png")
+    if nx.algorithms.components.connected.is_connected(G):
+        #print "yay, graph is connected"
+        pass
+    else:
+        #print "no no no, graph is not connected"
+        sys.exit(0)
+
+    """
+    source = randint(0,F)    
+    plt.cla()
+    tree   = nx.algorithms.traversal.depth_first_search.dfs_tree(G,source)
+    nx.draw(tree)
+    plt.savefig("dfs_tree.png")
+
+    for node in nx.algorithms.traversal.depth_first_search.dfs_successors(G,source):
+        print node
+
+    S = partytime(G,source)
+    """
+
+    print T
+    print N
+    print F
+    print M
+    for (u,v) in G.edges_iter():
+        print "%d %d" % (u,v)
+    for _ in xrange(0,G.number_of_nodes()):
+        print "%d" % (randint(0,1000))
