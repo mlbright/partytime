c3388fb002272077e3273845d7245dd33b56e0cf
Martin-Louis Bright <mlbright@gmail.com>
2011-03-18 01:15:26 -0400

    working?

--- partytime.py
+++ partytime.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 
 import sys
+from collections import defaultdict
+from random import randint
 
 def _next_int():
     return int(sys.stdin.readline())
+
+class PartyTime(object):
+    
+    def __init__(self,G,costs,F):
+        self.cost = sum(costs)
+        self.G = G
+        self.F = F
+        self.friends = set(range(F))
+        self.best = set(G.keys())
+
+    def solve(self):
+        source = randint(0,self.F-1)
+        self.search(source,self.cost,set(),self.friends.copy())
+
+    def search(self,curr,cost,visited,friends):
+        if cost > self.cost:
+            return
+        if curr in friends:
+            friends.remove(curr)
+            if len(friends) == 0:
+                self.cost = cost
+                self.best = set(visited)
+                return
+        for next in self.G[curr]:
+            if next not in visited:
+                self.search(next,cost+self.cost[next],visited | set([next]),friends | set())
+
+    def output():
+        print "%d %d" % (self.cost,len(self.best))
+
 
 if __name__ == "__main__":
 
@@ -14,13 +46,20 @@ if __name__ == "__main__":
         M = _next_int()
 
         #print "%d %d %d" % (N,F,M)    
+        G = defaultdict(set)
         for _ in xrange(M):
             u,v = sys.stdin.readline().strip().split()
             u,v = int(u),int(v)
-            print "%d %d" % (u,v)
+            #print "%d %d" % (u,v)
+            G[u].add(v)
+            G[v].add(u)
+            
+        #print G
 
         costs = []
         for _ in xrange(N):
-            costs.append(int(sys.stdin.readline()))
+            costs.append(int(_next_int()))
 
-        print costs
+        p  = PartyTime(G,costs,F)
+        p.solve()
+        p.output()
