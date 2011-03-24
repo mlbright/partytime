181132664b4682df5274a90e67fe0ea1ec63c1cd
Martin-Louis Bright <mlbright@gmail.com>
2011-03-24 15:13:20 -0400

    back to the drawing board

--- partytime.py
+++ partytime.py
@@ -7,28 +7,24 @@ from random import randint
 def _next_int():
     return int(sys.stdin.readline())
 
+def _collapse(sink,graph,path):
+    G = defaultdict(set)
+    for s in graph:
+        for d in graph[s]:
+            if s not in path and d not in path:
+                G[s].add(d)
+            elif s not in path and d in path:
+                G[s].add(sink)
+            elif s in path and d not in path:
+                G[sink].add(d)
+    return G
+
 class PartyTime(object):
     
     def __init__(self,G,costs,F):
         self.G = G
         self.food = costs
         self.F = F
-
-    def _collapse(self,graph,sol,path):
-
-        sink  = self.start
-        G = defaultdict(set)
-
-        for s in graph:
-            for d in graph[s]:
-                if s not in path and d not in path:
-                    G[s].add(d)
-                elif s not in path and d in path:
-                    G[s].add(sink)
-                elif s in path and d not in path:
-                    G[sink].add(d)
-    
-        return G, sol | path
 
     def solve(self):
         self.friends = set(range(self.F))
@@ -38,23 +34,15 @@ class PartyTime(object):
         self.best = set(self.G.keys())
         self._search(self.G,self.start)
 
-    def _search(self,graph,curr,cost=0,sol=set(),path=set()):
-        if cost >= self.cost:
-            return
-        if curr in self.friends and path:
-            print "here"
-            if self.friends.issubset(sol):
-                if cost <= self.cost:
-                    self.cost = cost
-                    self.best = set(sol)
-                return
-            _graph,_sol = self._collapse(graph,sol,path)
-            self._search(_graph,self.start,cost,_sol)
-        else:
-            for next in graph[curr]:
-                if next not in path:
-                    self._search(graph,next,cost+self.food[curr],sol,path|set([curr]))
-
+    def _search(self,graph,curr,visited=set(),sol=set(),path=set()):
+        visited.add(curr)
+        for next in graph[curr]:
+            """
+            if next in self.friends:
+                self._search(_collapse(self.start,graph,path),self.start)
+            """
+            if next not in visited:
+                self._search(graph,next,visited,sol,path|set([curr]))
 
     def output(self):
         print self.best
